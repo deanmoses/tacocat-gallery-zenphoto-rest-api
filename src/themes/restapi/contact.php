e503f92b8a715c281c844e69c71726066606b503
Dean <dmoses@salesforce.com>
2014-11-25 23:24:06 -0800

    initial code commit

---
+++ src/themes/restapi/contact.php
@@ -0,0 +1,44 @@
+<?php
+// force UTF-8 Ø
+
+if (!defined('WEBPATH'))
+	die();
+if (function_exists('printContactForm')) {
+	?>
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<?php zp_apply_filter('theme_head'); ?>
+			<?php printHeadTitle(); ?>
+			<meta charset="<?php echo LOCAL_CHARSET; ?>">
+			<meta http-equiv="content-type" content="text/html; charset=<?php echo LOCAL_CHARSET; ?>" />
+			<link rel="stylesheet" href="<?php echo pathurlencode($zenCSS); ?>" type="text/css" />
+			<link rel="stylesheet" href="<?php echo pathurlencode(dirname(dirname($zenCSS))); ?>/common.css" type="text/css" />
+		</head>
+		<body>
+			<?php zp_apply_filter('theme_body_open'); ?>
+			<div id="main">
+				<div id="gallerytitle">
+					<h2>
+						<?php printHomeLink('', ' | '); ?>
+						<a href="<?php echo html_encode(getGalleryIndexURL()); ?>" title="<?php echo gettext('Gallery Index'); ?>"><?php echo gettext('Gallery Index'); ?></a> |
+						<em><?php echo gettext('Contact us'); ?></em>
+					</h2>
+				</div>
+				<h3><?php echo gettext('Contact us.') ?></h3>
+				<?php printContactForm(); ?>
+			</div>
+			<?php @call_user_func('printLanguageSelector'); ?>
+			<div id="credit">
+				<?php printZenphotoLink(); ?>
+			</div>
+			<?php
+			zp_apply_filter('theme_body_close');
+			?>
+		</body>
+	</html>
+	<?php
+} else {
+	include(SERVERPATH . '/' . ZENFOLDER . '/404.php');
+}
+?>
---
+++ src/themes/restapi/password.php
@@ -0,0 +1,47 @@
+<?php
+// force UTF-8 Ø
+
+if (!defined('WEBPATH'))
+	die();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php zp_apply_filter('theme_head'); ?>
+		<?php printHeadTitle(); ?>
+		<meta charset="<?php echo LOCAL_CHARSET; ?>">
+		<meta http-equiv="content-type" content="text/html; charset=<?php echo LOCAL_CHARSET; ?>" />
+		<link rel="stylesheet" href="<?php echo pathurlencode($zenCSS); ?>" type="text/css" />
+		<link rel="stylesheet" href="<?php echo pathurlencode(dirname(dirname($zenCSS))); ?>/common.css" type="text/css" />
+		head>
+	<body>
+		<?php zp_apply_filter('theme_body_open'); ?>
+		<div id="main">
+			<div id="gallerytitle">
+				<h2>
+					<span>
+						<?php printHomeLink('', ' | '); ?>
+						<a href="<?php echo html_encode(getGalleryIndexURL()); ?>" title="<?php echo gettext('Gallery Index'); ?>"><?php printGalleryTitle(); ?></a>
+					</span> |
+					<?php echo gettext("A password is required for the page you requested"); ?>
+				</h2>
+			</div>
+			<div id="padbox">
+				<?php printPasswordForm($hint, $show, false); ?>
+			</div>
+		</div>
+		<div id="credit">
+			<?php
+			if (!zp_loggedin() && function_exists('printRegisterURL') && $_zp_gallery->isUnprotectedPage('register')) {
+				echo '<p>';
+				printRegisterURL(gettext('Register for this site'), '<br />');
+				echo '</p>';
+			}
+			?>
+			<?php printZenphotoLink(); ?>
+		</div>
+		<?php
+		zp_apply_filter('theme_body_close');
+		?>
+	</body>
+</html>
